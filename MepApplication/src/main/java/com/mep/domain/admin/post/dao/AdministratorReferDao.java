f31b31e30a162a197242185fd6fa774d1faf4e9b
yewin <yewin29081990@gmail.com>
2017-08-03 15:42:09 +0630

    Deleted unnecessary files and code.

--- MepApplication/src/main/java/com/mep/domain/admin/post/dao/AdministratorReferDao.java
+++
@@ -1,8 +0,0 @@
-package com.mep.domain.admin.post.dao;
-
-import com.mep.database.entity.Administrator;
-
-public interface AdministratorReferDao {
-
-	Administrator getAdminIdByEmail(String admin_email);
-}
